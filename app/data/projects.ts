98b33a44b606f0aebc1e8733e3acaf5bb0c51b1a
RukshanPokharel <rukshanpokharel@gmail.com>
2025-04-23 01:11:17 +0200

    Add a new project reference

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -6,15 +6,24 @@ export const projects = [
     image: "/portfolio.jpg",
     tech: ["Next.js", "Tailwind CSS", "Framer Motion"],
     github: "https://github.com/RukshanPokharel/My-Portfolio-Next.js-",
-    demo: "/",
+    demo: "https://my-portfolio-next-js-lyart-five.vercel.app/",
   },
   {
     title: "Restaurant Management System",
     description: "An advance restaurant management system under development",
     image: "/reservation.png",
-    tech: ["NextJS", "PostGreSQL", "tailwind"],
+    tech: ["NextJS", "PostgresSQL", "tailwind"],
     github: "https://github.com/RukshanPokharel/rms-advanced",
     demo: "https://rms-advanced.vercel.app/home",
+  },
+  {
+    title: "Next JS fundamentals tutorial project",
+    description:
+      "A tutorial project by Next JS itself where I learned the core features of Next.js by building a full-stack web application. Followed every step and covered every topics by hands on coding. ",
+    image: "/next-tutorial.png",
+    tech: ["NextJS", "PostgresSQL", "tailwind", "App-router"],
+    github: "https://github.com/RukshanPokharel/nextjs-dashboard-demo",
+    demo: "https://next-learn-dashboard.vercel.sh/",
   },
   // Add more projects...
 ];
